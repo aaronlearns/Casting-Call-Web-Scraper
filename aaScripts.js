de0c23521c97cd8796ddadf15fecc887b4c66f16
Aaron Greenberg <aaronlearns39@gmail.com>
2021-10-13 23:22:07 -0400

    reattaching js scripts from actorsaccess assets

---
+++ aaScripts.js
@@ -0,0 +1,17 @@
+// This is a script extracted from the actorsaccess assets folder. It's the onclick= attribute
+// for the anchor element on each page.
+
+function selectPhoto(iid, bid, el) {
+    var editcart = "";
+    var winl = (screen.width - 800) / 2;
+    var wint = (screen.height - 600) / 2;
+    winprops = 'top='+wint+',left='+winl;
+    if (typeof el !== 'undefined' && el.tagName == 'A' && el.text.indexOf('CHANGE PHOTO') > -1){
+    editcart = "&editcart=1";
+    }
+    window.open('/projects/?view=selectphoto&from=breakdowns&region=32&iid=' + iid + '&bid=' + bid + editcart, 'select_photo', 'scrollbars,resizable,width=800,height=600,' + winprops);
+    }
+    function remove(iid) {
+    redirect_url = '/projects/?view=breakdowns&breakdown=724155&region=32&removeitem=' + iid;
+    location.replace(redirect_url);
+    }
