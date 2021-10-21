969fb95f301b5d90ff4a3601f09ba52ef6577624
Aaron Greenberg <aaronlearns39@gmail.com>
2021-10-21 11:27:04 -0400

    Finished declaring class attributes

--- RoleCall.py
+++ RoleCall.py
@@ -1,16 +1,24 @@
+from datetime import datetime as dt
+
 UNIONS = ["AABP", "SAG-AFTRA", "SAG-AFTRA Eligible", "AEA", "AEA (EMC)", "ACTRA", "ACTRA Apprentice", "AGVA", "AGMA", "BAE", "AAE", "BECTU", "CAEA", "CAEA Apprentice", "UBCP/ACTRA", "UBCP/ACTRA Apprentice", "Financial Core", "UDA", "UDA/ACTRA", "UDA Stagiaire","NON-UNION"]
 
+class Role():
+    def __init__(self):
+        self.name = "" # The name of the character
+        self.descripton = "" # Character breakdown/descripton
+        self.gender = "" # Character gender STRING
+        self.race = "" # Character race STRING
+        self.ageRange = [] # Character ageRange LIST of INTS
+        self.height = 0 # Character height in inches INT
 
-
-class call():
-    def __init__(self,roles=[],dateTime="",location="",medium="",payRate=0.0,postDateTime="",union=""):
-        self.roles = roles # A list of role objects.
-        self.dateTime = dateTime # Timestamp of runtime retrieval of data
-        self.location = location # Location of job
-        self.medium = medium # Film, TV or theatre
-        self.payRate = payRate # 2-long tuple: (FLOAT $00.00, STRING per "day","performance" etc.)
-        self.postDateTime = postDateTime # Timestamp of the time the call was posted.
-        self.union = union # SAG-AFTRA, UAE etc.
-    
-newCall = call([])
-print(newCall.medium)
+# dateTime IS THE RUNTIME RETRIEVAL OF THE CALL, WHILE postDateTime IS THE TIME THE CALL WAS POSTED.
+class Call():
+    def __init__(self):
+        self.roles = [] # A list of role objects.
+        self.dateTime = dt.now() # Timestamp of runtime retrieval of data DATETIME
+        self.location = "" # Location of job STRING
+        self.medium = "" # Film, TV, Theatre, Voiceover etc. STRING
+        self.name = "" # Name of the project STRING
+        self.payRate = [0.0,""] # 2-long list: [per-diem $ rate FLOAT, the diem: "day","performance" etc. STRING]
+        self.postDateTime = "" # Timestamp of the time the call was posted. STRING
+        self.union = "" # SAG-AFTRA, UAE etc. STRING
