d85821040a8ec670b34bb2a2734483ac0f3d4de3
Aaron Greenberg <aaronlearns39@gmail.com>
2021-10-22 03:57:02 -0400

    Got selectPhoto to run in Node successfully.

--- aaScripts.js
+++ aaScripts.js
@@ -1,16 +1,23 @@
 
-// Onclick= attribute for every <a> tag displaying the character's name. Opens the window
+
+// Onclick= attribute for every <a> tag displaying the character's name. Opens the window to apply for the gig. Long term goal is to put this in the email.
 function selectPhoto(iid, bid, el) {
-    var editcart = "";
-    var winl = (screen.width - 800) / 2;
-    var wint = (screen.height - 600) / 2;
-    winprops = 'top='+wint+',left='+winl;
-    if (typeof el !== 'undefined' && el.tagName == 'A' && el.text.indexOf('CHANGE PHOTO') > -1){
-    editcart = "&editcart=1";
+    if (typeof(window) !== "undefined") {
+        var editcart = "";
+        var winl = (screen.width - 800) / 2;
+        var wint = (screen.height - 600) / 2;
+        winprops = 'top='+wint+',left='+winl;
+        if (typeof el !== 'undefined' && el.tagName == 'A' && el.text.indexOf('CHANGE PHOTO') > -1){
+        editcart = "&editcart=1";
+        }
+        window.open('/projects/?view=selectphoto&from=breakdowns&region=32&iid=' + iid + '&bid=' + bid + editcart, 'select_photo', 'scrollbars,resizable,width=800,height=600,' + winprops);
     }
-    window.open('/projects/?view=selectphoto&from=breakdowns&region=32&iid=' + iid + '&bid=' + bid + editcart, 'select_photo', 'scrollbars,resizable,width=800,height=600,' + winprops);
-    }
-    function remove(iid) {
+    return 0;
+}
+function remove(iid) {
     redirect_url = '/projects/?view=breakdowns&breakdown=724155&region=32&removeitem=' + iid;
     location.replace(redirect_url);
-    }
+}
+selectPhoto(4512834,725387)
+// print()
+// javascript: selectPhoto(4512834,725387);
